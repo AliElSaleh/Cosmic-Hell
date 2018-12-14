8ef6dc0911a6b9dd9c559bcb11554f832a5028eb
Ali El Saleh <elsaleh78@gmail.com>
2018-12-14 15:01:44 +1100

    Implement Game states
    Re-organise variables in Main.h

--- FlockingHell/FlockingHell/Main.h
+++ FlockingHell/FlockingHell/Main.h
@@ -4,6 +4,7 @@
 #include "Player.h"
 #include "Bullet.h"
 #include "Star.h"
+#include "State.h"
 
 #define MAX_SMALL_BULLETS 20
 #define MAX_PLAYER_BULLETS 50
@@ -11,14 +12,23 @@
 
 // Variable Declarations
 //-----------------------------------------
+// Structs
 struct Player Player;
 struct Bullet Bullet[MAX_SMALL_BULLETS];
 struct Star SmallStars[MAX_SMALL_STARS];
+struct Vector2 BulletLocation[MAX_SMALL_BULLETS];
+enum State GameState;
+
+// ints
 const int SCREEN_WIDTH = 700;
 const int SCREEN_HEIGHT = 800;
-bool bShowDebugHit = false;
 int ShootRate = 0;
 int Offset = 0;
+int FramesCounter = 0;
+
+// bools
+bool bShowDebugHit = false;
+
 
 // Function Declarations
 //-----------------------------------------
@@ -34,3 +44,9 @@ void CheckPlayerHealth();
 void UpdatePlayerBullet();
 void UpdateStars();
 void CheckStarsOutsideWindow();
+void UpdateSplashScreen();
+void DrawSplashScreen();
+void UpdateGame();
+void DrawGame();
+void UpdateMenu();
+void DrawMenu();
---
+++ FlockingHell/FlockingHell/State.h
@@ -0,0 +1,14 @@
+#pragma once
+
+enum State
+{
+	SPLASH = 0,
+	MENU = 1,
+	GAME = 2,
+	PAUSE = 3,
+	DEATH = 4,
+	WIN = 5,
+	LOSE = 6,
+	STATS = 7,
+	HIGHSCORE = 8
+};
