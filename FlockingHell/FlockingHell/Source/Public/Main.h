82260aa6b37ad1e01821f6d9f0c5089290674240
Ali El Saleh <elsaleh78@gmail.com>
2019-02-10 21:12:15 +1100

    Fix an issue where the menu switch sound would play over and over again really fast

--- FlockingHell/FlockingHell/Source/Public/Main.h
+++ FlockingHell/FlockingHell/Source/Public/Main.h
@@ -89,7 +89,7 @@ char Buffer[4096];
 unsigned short ReconnectDelay{7200}; // 1 minute (in frames)
 
 // bools
-bool bSoundPlayed{false};
+bool bSoundPlayed[5]{false};
 bool bEntered{false}; // To connect immediately upon entering the leaderboard game state
 bool bConnected{false};
 bool bBegan{false};
